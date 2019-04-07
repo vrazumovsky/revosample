a75c921f2199c060751213a36006d6d6e9b7fbce
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-07 11:44:53 +0300

    added empty CurrencyChangeFragment

---
+++ app/src/main/java/ru/razumovsky/sampleapp/screens/main/currencychange/CurrencyChangeFragment.kt
@@ -0,0 +1,23 @@
+package ru.razumovsky.sampleapp.screens.main.currencychange
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.razumovsky.sampleapp.R
+
+
+class CurrencyChangeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() : CurrencyChangeFragment {
+            return CurrencyChangeFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.currency_change_fragment, container, false)
+        return view
+    }
+}
