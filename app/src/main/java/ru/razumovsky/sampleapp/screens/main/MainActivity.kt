bc1cc125b65adc5d374c64983bdc1e7aa9568b52
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-07 12:35:22 +0300

    assemble Main module and call presenter.onCreate()

--- app/src/main/java/ru/razumovsky/sampleapp/screens/main/MainActivity.kt
+++ app/src/main/java/ru/razumovsky/sampleapp/screens/main/MainActivity.kt
@@ -7,8 +7,19 @@ import ru.razumovsky.sampleapp.core.ui.BaseRouterView
 
 class MainActivity : AppCompatActivity(), MainView, BaseRouterView {
 
+    private lateinit var presenter: MainPresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        assemble()
+        presenter.onCreate()
+
+    }
+
+    private fun assemble() {
+        val router = MainRouterImpl(this, R.id.content)
+        presenter = MainPresenterImpl(this, router)
     }
 }
