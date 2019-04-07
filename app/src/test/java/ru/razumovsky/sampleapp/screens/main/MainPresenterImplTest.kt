907bba4c16c37fb6da90422c551e32576365a2f6
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-07 12:22:18 +0300

    MainPresenterImplTest added with 1 test

---
+++ app/src/test/java/ru/razumovsky/sampleapp/screens/main/MainPresenterImplTest.kt
@@ -0,0 +1,32 @@
+package ru.razumovsky.sampleapp.screens.main
+
+import com.nhaarman.mockito_kotlin.verify
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+class MainPresenterTest {
+
+    @Mock
+    private lateinit var mockView: MainView
+
+    @Mock
+    private lateinit var mockRouter: MainRouter
+
+    private lateinit var presenter: MainPresenter
+
+    @Before
+    fun initialize() {
+        MockitoAnnotations.initMocks(this)
+
+        presenter = MainPresenterImpl(mockView, mockRouter)
+    }
+
+
+    @Test
+    fun `presenter onCreate called, should call router open currency change screen`() {
+        presenter.onCreate()
+        verify(mockRouter).openCurrencyChangeInfo()
+    }
+}
