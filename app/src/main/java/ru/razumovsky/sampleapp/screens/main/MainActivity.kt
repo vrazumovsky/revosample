46020878076040531423ec3e40fecb6915940925
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-07 15:22:31 +0300

    build component and inject presenter in MainActivity

--- app/src/main/java/ru/razumovsky/sampleapp/screens/main/MainActivity.kt
+++ app/src/main/java/ru/razumovsky/sampleapp/screens/main/MainActivity.kt
@@ -4,22 +4,34 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import ru.razumovsky.sampleapp.R
 import ru.razumovsky.sampleapp.core.ui.BaseRouterView
+import ru.razumovsky.sampleapp.screens.main.di.DaggerMainComponent
+import ru.razumovsky.sampleapp.screens.main.di.MainModule
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), MainView, BaseRouterView {
 
-    private lateinit var presenter: MainPresenter
+    @Inject
+    lateinit var presenter: MainPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        assemble()
+        buildComponent()
         presenter.onCreate()
 
     }
 
-    private fun assemble() {
-        val router = MainRouterImpl(this, R.id.content)
-        presenter = MainPresenterImpl(this, router)
+
+    private fun buildComponent() {
+        DaggerMainComponent.builder()
+            .mainModule(
+                MainModule(
+                    view = this,
+                    routerView = this,
+                    containerId = R.id.content
+                )
+            )
+            .build().inject(this)
     }
 }
