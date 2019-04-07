eb9c4491b647331feac21ab887e28e360ee6cd67
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-07 12:16:53 +0300

    Main router openCurrencyChangeInfo implemented replaceFragment method added

--- app/src/main/java/ru/razumovsky/sampleapp/screens/main/MainRouterImpl.kt
+++ app/src/main/java/ru/razumovsky/sampleapp/screens/main/MainRouterImpl.kt
@@ -1,11 +1,23 @@
 package ru.razumovsky.sampleapp.screens.main
 
+import android.support.v4.app.Fragment
 import ru.razumovsky.sampleapp.core.ui.BaseRouterView
+import ru.razumovsky.sampleapp.screens.main.currencychange.CurrencyChangeFragment
 
-class MainRouterImpl(private val view: BaseRouterView) : MainRouter {
+class MainRouterImpl(
+    private val view: BaseRouterView,
+    private val containerId: Int
+) : MainRouter {
+
     override fun openCurrencyChangeInfo() {
-
+        val fragment = CurrencyChangeFragment.newInstance()
+        replace(fragment)
     }
 
-    
+    private fun replace(fragment: Fragment) {
+        view.getSupportFragmentManager()
+            .beginTransaction()
+            .replace(containerId, fragment)
+            .commit()
+    }
 }
