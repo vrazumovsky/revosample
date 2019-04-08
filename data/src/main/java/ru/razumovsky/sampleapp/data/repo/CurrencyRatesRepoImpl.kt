0342e33750a26716f246443ab5bb0d99034b2a7c
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-08 12:25:43 +0300

    CurrencyRatesRepoImpl not finished implementation added

---
+++ data/src/main/java/ru/razumovsky/sampleapp/data/repo/CurrencyRatesRepoImpl.kt
@@ -0,0 +1,15 @@
+package ru.razumovsky.sampleapp.data.repo
+
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.subscribeBy
+import ru.razumovsky.sampleapp.data.entity.CurrencyRate
+import ru.razumovsky.sampleapp.data.network.request.CurrencyRatesRequest
+import javax.inject.Inject
+
+class CurrencyRatesRepoImpl @Inject constructor(private val request: CurrencyRatesRequest) : CurrencyRatesRepo {
+
+    override fun getRates(): Observable<List<CurrencyRate>> {
+        return Observable.empty()
+    }
+
+}
