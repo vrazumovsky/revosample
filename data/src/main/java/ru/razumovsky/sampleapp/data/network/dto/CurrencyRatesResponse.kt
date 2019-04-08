8041ec4276ad85b79fe58dcb9bb0c5f79873cb3d
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-08 12:21:09 +0300

    CurrencyRatesResponse added

---
+++ data/src/main/java/ru/razumovsky/sampleapp/data/network/dto/CurrencyRatesResponse.kt
@@ -0,0 +1,14 @@
+package ru.razumovsky.sampleapp.data.network.dto
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class CurrencyRatesResponse(
+    @SerializedName("base") val base: String?,
+    @SerializedName("date") val date: String?,
+
+    @SerializedName("rates")
+    @Expose
+    val rates: Map<String, Float>
+
+)
