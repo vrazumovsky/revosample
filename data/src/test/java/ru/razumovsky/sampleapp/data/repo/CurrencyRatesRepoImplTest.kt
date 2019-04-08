5797794549da4291c760ffdbbcbde1cd3b6fa89e
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-08 12:39:51 +0300

    fix repo tests

--- data/src/test/java/ru/razumovsky/sampleapp/data/repo/CurrencyRatesRepoImplTest.kt
+++ data/src/test/java/ru/razumovsky/sampleapp/data/repo/CurrencyRatesRepoImplTest.kt
@@ -1,10 +1,15 @@
 package ru.razumovsky.sampleapp.data.repo
 
+import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Observable
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import ru.razumovsky.sampleapp.data.entity.Currency
+import ru.razumovsky.sampleapp.data.network.dto.CurrencyRatesResponse
 import ru.razumovsky.sampleapp.data.network.request.CurrencyRatesRequest
 
 class CurrencyRatesRepoImplTest {
@@ -14,10 +19,23 @@ class CurrencyRatesRepoImplTest {
 
     private lateinit var repo: CurrencyRatesRepo
 
+    private lateinit var mockResponse: CurrencyRatesResponse
+
+    private val mockDate = "03-03-2019"
+
+    private val mockRates = mapOf(
+        Pair(Currency.EUR.value, 1.4f),
+        Pair(Currency.AUD.value, 1.3f),
+        Pair(Currency.USD.value, 1.1f)
+    )
+
 
     @Before
     fun initialize() {
         MockitoAnnotations.initMocks(this)
+
+        mockResponse = CurrencyRatesResponse(Currency.EUR.value, mockDate, mockRates)
+        whenever(mockRequest.run()).thenReturn(Observable.just(mockResponse))
 
         repo = CurrencyRatesRepoImpl(mockRequest)
     }
