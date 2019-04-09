1f022ffb61910966285ff2f87eb83baa61412217
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-09 15:52:21 +0300

    added verbose name to CurrencyItem

--- app/src/main/java/ru/razumovsky/sampleapp/screens/main/currencychange/CurrencyItem.kt
+++ app/src/main/java/ru/razumovsky/sampleapp/screens/main/currencychange/CurrencyItem.kt
@@ -3,6 +3,7 @@ package ru.razumovsky.sampleapp.screens.main.currencychange
 import com.github.nitrico.lastadapter.StableId
 
 data class CurrencyItem(val name: String,
+                        val verboseName: String,
                         val rate: Float) : StableId {
     override val stableId: Long
         get() = name.hashCode().toLong() + rate.toLong()
