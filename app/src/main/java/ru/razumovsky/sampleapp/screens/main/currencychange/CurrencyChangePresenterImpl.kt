f317b533e2d0c733bb156fd1eb607ddc6dd92fdb
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-09 15:57:11 +0300

    simple example of showing currency list implemented

--- app/src/main/java/ru/razumovsky/sampleapp/screens/main/currencychange/CurrencyChangePresenterImpl.kt
+++ app/src/main/java/ru/razumovsky/sampleapp/screens/main/currencychange/CurrencyChangePresenterImpl.kt
@@ -1,12 +1,28 @@
 package ru.razumovsky.sampleapp.screens.main.currencychange
 
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.subscribeBy
+import ru.razumovsky.sampleapp.domain.usecase.GetCurrencyRatesUseCase
 import javax.inject.Inject
 
 class CurrencyChangePresenterImpl @Inject constructor(
-    private val view: CurrencyChangeView
+    private val view: CurrencyChangeView,
+    private val mapper: CurrencyRateUIMapper,
+    private val useCase: GetCurrencyRatesUseCase
 ) : CurrencyChangePresenter {
-    override fun onReady() {
 
+    override fun onReady() {
+        useCase.execute()
+            .map { mapper.map(it) }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onNext = {
+                    view.showCurrencies(it)
+                },
+                onError = {
+                    it.printStackTrace()
+                }
+            )
     }
 
 }
