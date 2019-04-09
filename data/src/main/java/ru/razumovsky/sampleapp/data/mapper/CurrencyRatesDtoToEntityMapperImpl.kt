1e2e635348335a2946ddc5f965d0066c8f59132f
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-09 16:05:01 +0300

    added inject annotation to mapper constructor

--- data/src/main/java/ru/razumovsky/sampleapp/data/mapper/CurrencyRatesDtoToEntityMapperImpl.kt
+++ data/src/main/java/ru/razumovsky/sampleapp/data/mapper/CurrencyRatesDtoToEntityMapperImpl.kt
@@ -2,8 +2,9 @@ package ru.razumovsky.sampleapp.data.mapper
 
 import ru.razumovsky.sampleapp.data.entity.CurrencyRates
 import ru.razumovsky.sampleapp.data.network.dto.CurrencyRatesResponse
+import javax.inject.Inject
 
-class CurrencyRatesDtoToEntityMapperImpl : CurrencyRatesDtoToEntityMapper {
+class CurrencyRatesDtoToEntityMapperImpl @Inject constructor() : CurrencyRatesDtoToEntityMapper {
     override fun map(dto: CurrencyRatesResponse): CurrencyRates {
         return CurrencyRates(
             base = dto.base ?: "",
