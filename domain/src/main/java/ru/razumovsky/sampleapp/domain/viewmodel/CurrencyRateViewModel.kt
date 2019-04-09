0ed518183311f56af31189e8a7f764f949cb7ede
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-09 15:50:59 +0300

    CurrencyRateViewModel added

---
+++ domain/src/main/java/ru/razumovsky/sampleapp/domain/viewmodel/CurrencyRateViewModel.kt
@@ -0,0 +1,5 @@
+package ru.razumovsky.sampleapp.domain.viewmodel
+
+class CurrencyRateViewModel(val name: String,
+                            val verboseName: String,
+                            val value: Float)
