8c443bac8c1dad84e43aa737ca013b718c000497
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-09 15:51:48 +0300

    UNKNOWN Currency added. Added find() method to Currency

--- data/src/main/java/ru/razumovsky/sampleapp/data/entity/Currency.kt
+++ data/src/main/java/ru/razumovsky/sampleapp/data/entity/Currency.kt
@@ -3,10 +3,16 @@ package ru.razumovsky.sampleapp.data.entity
 enum class Currency(val value: String,
                     val verboseName: String) {
 
+
     USD("USD", "US Dollar"),
     EUR("EUR", "Euro"),
     AUD("AUD", "Australian Dollar"),
     BGN("BGN", "Bulgarian lev"),
+    UNKNOWN("UNKNOWN", "Unknown");
+
+    companion object {
+        fun find(value: String): Currency = Currency.values().find { it.value == value } ?: UNKNOWN
+    }
 
     //todo add missing currencies
 }
