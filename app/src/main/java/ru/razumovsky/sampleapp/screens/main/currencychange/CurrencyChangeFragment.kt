a3874c6bfaee37ded80c61a50bbdaa91bbb32769
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-09 16:00:54 +0300

    added last adapter to CurrencyChangeFragment showCurrencies implemented

--- app/src/main/java/ru/razumovsky/sampleapp/screens/main/currencychange/CurrencyChangeFragment.kt
+++ app/src/main/java/ru/razumovsky/sampleapp/screens/main/currencychange/CurrencyChangeFragment.kt
@@ -1,10 +1,16 @@
 package ru.razumovsky.sampleapp.screens.main.currencychange
 
+import android.databinding.ObservableArrayList
+import android.databinding.ObservableList
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.nitrico.lastadapter.LastAdapter
+import com.github.nitrico.lastadapter.StableId
+import kotlinx.android.synthetic.main.currency_change_fragment.*
+import ru.razumovsky.sampleapp.BR
 import ru.razumovsky.sampleapp.R
 import javax.inject.Inject
 
@@ -17,6 +23,12 @@ class CurrencyChangeFragment : Fragment(), CurrencyChangeView {
         }
     }
 
+    private val items: ObservableList<StableId> = ObservableArrayList<StableId>()
+
+    private val adapter: LastAdapter = LastAdapter(items, BR.item, stableIds = true)
+
+        .map<CurrencyItem>(R.layout.currency_item)
+
 
     @Inject
     lateinit var presenter: CurrencyChangePresenter
@@ -28,7 +40,13 @@ class CurrencyChangeFragment : Fragment(), CurrencyChangeView {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        adapter.into(recyclerView)
         presenter.onReady()
 
+    }
+
+    override fun showCurrencies(data: List<StableId>) {
+        items.clear()
+        items.addAll(data)
     }
 }
