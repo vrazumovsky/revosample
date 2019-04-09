9db26261844801eb6e352fe02ba7e1fb3819f231
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-09 16:08:48 +0300

    added dependency to MainComponent and plus method

--- app/src/main/java/ru/razumovsky/sampleapp/screens/main/di/MainComponent.kt
+++ app/src/main/java/ru/razumovsky/sampleapp/screens/main/di/MainComponent.kt
@@ -1,12 +1,16 @@
 package ru.razumovsky.sampleapp.screens.main.di
 
 import dagger.Component
+import ru.razumovsky.sampleapp.di.components.AppComponent
 import ru.razumovsky.sampleapp.di.scope.ActivityScope
 import ru.razumovsky.sampleapp.screens.main.MainActivity
+import ru.razumovsky.sampleapp.screens.main.currencychange.di.CurrencyChangeComponent
+import ru.razumovsky.sampleapp.screens.main.currencychange.di.CurrencyChangeModule
 
 @ActivityScope
-@Component(modules = [MainModule::class])
+@Component(modules = [MainModule::class], dependencies = [AppComponent::class])
 interface MainComponent {
     fun inject(view: MainActivity)
 
+    fun plus(module: CurrencyChangeModule): CurrencyChangeComponent
 }
