7b8ffcf3c550c3d47f1a47ff93a1bd99fc49288a
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-24 11:01:04 +0300

    fixed parsing number issue on some phones

--- app/src/main/java/ru/razumovsky/sampleapp/screens/main/currencychange/CurrencyRateUIMapperImpl.kt
+++ app/src/main/java/ru/razumovsky/sampleapp/screens/main/currencychange/CurrencyRateUIMapperImpl.kt
@@ -6,7 +6,7 @@ import javax.inject.Inject
 
 class CurrencyRateUIMapperImpl @Inject constructor(): CurrencyRateUIMapper {
     override fun map(list: List<CurrencyRateViewModel>): List<StableId> = list.mapIndexed { index, it ->
-        val value = if (it.value == 0f) "" else String.format("%.2f", it.value)
+        val value = if (it.value == 0f) "" else String.format("%.2f", it.value).replace(",", ".")
         val currencyInfo = CurrencyInfo.find(it.name)
         CurrencyItem(it.name, currencyInfo.verboseName, currencyInfo.imageResource, value)
     }
