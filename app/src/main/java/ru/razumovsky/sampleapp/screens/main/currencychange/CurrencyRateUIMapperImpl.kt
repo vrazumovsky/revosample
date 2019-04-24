9274beac1784c69c57ee1013cc99a125df71dc5e
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-24 12:01:38 +0300

    improve rate value format performance

--- app/src/main/java/ru/razumovsky/sampleapp/screens/main/currencychange/CurrencyRateUIMapperImpl.kt
+++ app/src/main/java/ru/razumovsky/sampleapp/screens/main/currencychange/CurrencyRateUIMapperImpl.kt
@@ -2,11 +2,20 @@ package ru.razumovsky.sampleapp.screens.main.currencychange
 
 import com.github.nitrico.lastadapter.StableId
 import ru.razumovsky.sampleapp.domain.viewmodel.CurrencyRateViewModel
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 import javax.inject.Inject
 
 class CurrencyRateUIMapperImpl @Inject constructor(): CurrencyRateUIMapper {
     override fun map(list: List<CurrencyRateViewModel>): List<StableId> = list.mapIndexed { index, it ->
-        val value = if (it.value == 0f) "" else String.format("%.2f", it.value).replace(",", ".")
+        val otherSymbols = DecimalFormatSymbols()
+        otherSymbols.decimalSeparator = '.'
+        val formatter = DecimalFormat("#.##", otherSymbols)
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+
+        val value = if (it.value == 0f) "" else formatter.format(it.value)
         val currencyInfo = CurrencyInfo.find(it.name)
         CurrencyItem(it.name, currencyInfo.verboseName, currencyInfo.imageResource, value)
     }
