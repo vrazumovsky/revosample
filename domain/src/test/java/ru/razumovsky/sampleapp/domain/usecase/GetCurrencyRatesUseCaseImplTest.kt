845445af5715c472f3afaae5fb2d57d6f59a5442
Vadim Matsishin <matsishin.vadim@gmail.com>
2019-04-29 09:05:05 +0300

    added usecase unit tests

---
+++ domain/src/test/java/ru/razumovsky/sampleapp/domain/usecase/GetCurrencyRatesUseCaseImplTest.kt
@@ -0,0 +1,56 @@
+package ru.razumovsky.sampleapp.domain.usecase
+
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.TestScheduler
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import ru.razumovsky.sampleapp.data.entity.Currency
+import ru.razumovsky.sampleapp.data.repo.CurrencyRatesRepo
+
+class GetCurrencyRatesUseCaseImplTest {
+
+    @Mock
+    private lateinit var mockRepo: CurrencyRatesRepo
+
+    private lateinit var testScheduler: TestScheduler
+
+    private lateinit var useCase: GetCurrencyRatesUseCase
+
+    private val mockRates = mapOf(
+        Pair(Currency.GBP.value, 1.4f),
+        Pair(Currency.AUD.value, 1.3f),
+        Pair(Currency.USD.value, 1.1f)
+    )
+
+    @Before
+    fun initialize() {
+        MockitoAnnotations.initMocks(this)
+
+        testScheduler = TestScheduler()
+        RxJavaPlugins.setComputationSchedulerHandler { testScheduler }
+
+        whenever(mockRepo.getRates()).thenReturn(Observable.just(mockRates))
+        whenever(mockRepo.getRatesSingle()).thenReturn(Single.just(mockRates))
+
+        useCase = GetCurrencyRatesUseCaseImpl(mockRepo)
+    }
+
+
+    @Test
+    fun `usecase executePolling() called,should call repo getRates()`() {
+        useCase.executePolling()
+        verify(mockRepo).getRates()
+    }
+
+    @Test
+    fun `usecase execute() called,should call repo getRatesSingle()`() {
+        useCase.execute()
+        verify(mockRepo).getRatesSingle()
+    }
+}
